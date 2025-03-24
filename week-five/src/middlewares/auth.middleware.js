4f6d2d73a4c53b536643d29b08356dfb3a61de99
Naresh Bohara <nareshbohara3305@gmail.com>
2025-03-24 10:29:04 +0545

    code updated

---
+++ week-five/src/middlewares/auth.middleware.js
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import jwt from "jsonwebtoken";
+const authenticateJWT = (req, res, next) => {
+    const token = req.headers.authorization?.split(" ")[2]; 
+    if (!token) return res.status(401).json({ message: "Access Denied" });
+    console.log("Token:", token);
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        console.error(error);
+        res.status(403).json({ message: "Invalid Token" });
+    }
+};
+
+export default authenticateJWT;
---
+++ week-five/src/routes/auth.router.js
@@ -0,0 +1,34 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js"
+
+import { Router } from "express";
+
+const authRouter = Router();
+
+const generateToken = (user) => {
+    return jwt.sign({ id: user._id, name: user.username }, process.env.JWT_SECRET, { expiresIn: "1h" });
+};
+
+authRouter.post("/register", async (req, res) => {
+    const { username, password } = req.body;
+    
+    const existingUser = await User.findOne({ username });
+    if (existingUser) return res.status(400).json({ message: "User already exists" });
+
+    const newUser = new User({ username, password });
+    await newUser.save();
+
+    res.json({ message: "User registered successfully" });
+});
+ 
+authRouter.post("/login", async (req, res) => {
+    const { username, password } = req.body;
+
+    const user = await User.findOne({ username });
+    if (!user || user.password !== password) return res.status(401).json({ message: "Invalid credentials" });
+
+    const token = generateToken(user);
+    res.json({ token });
+});
+
+export default authRouter;
