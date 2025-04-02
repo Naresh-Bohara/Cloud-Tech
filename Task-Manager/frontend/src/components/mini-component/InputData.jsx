3cd223718c812f4e6fb09e083a6488d0df21bf53
Naresh Bohara <nareshbohara3305@gmail.com>
2025-04-02 15:12:15 +0545

    code updated

--- Task-Manager/frontend/src/components/mini-component/InputData.jsx
+++ Task-Manager/frontend/src/components/mini-component/InputData.jsx
@@ -5,7 +5,9 @@ const InputData = () => {
     <>
       <div className="fixed top-0 left-0 bg-gray-200 opacity-80 h-screen w-full"></div>
       <div className="fixed top-0 left-0 flex items-center justify-center h-screen w-full">
-        <div className=""></div>
+        <div className="w-3/6 ">
+
+        </div>
       </div>
     </>
   );
